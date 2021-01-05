d73ca05d4fbdd550aec4b448f55978452cfe753a
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-01-05 10:56:48 -0300

    refactor: move sut creation to a factory method

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -6,9 +6,7 @@ import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 export class AuthMiddleware implements Middleware {
   constructor (
     private readonly loadAccountByToken: LoadAccountByToken
-  ) {
-
-  }
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const accessToken = httpRequest.headers?.['x-access-token']
