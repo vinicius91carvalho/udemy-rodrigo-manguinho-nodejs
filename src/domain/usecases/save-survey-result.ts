e3e85f402ff2e79ae456ece35a60b49ad8f858fc
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-01-14 17:06:31 -0300

    refactor: add data usecases protocol files to simplify imports

--- src/domain/usecases/save-survey-result.ts
+++ src/domain/usecases/save-survey-result.ts
@@ -3,5 +3,5 @@ import { SurveyResultModel } from '../models/survey-result'
 export type SaveSurveyResultModel = Omit<SurveyResultModel, 'id'>
 
 export interface SaveSurveyResult {
-  save: (data: SaveSurveyResultModel) => Promise<void>
+  save: (data: SaveSurveyResultModel) => Promise<SurveyResultModel>
 }
