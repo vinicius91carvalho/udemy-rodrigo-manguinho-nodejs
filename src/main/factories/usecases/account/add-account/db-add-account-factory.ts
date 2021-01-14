3cc16f74a1b09a8004dd59ec4ef7f18d80e68db7
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-01-14 20:20:35 -0300

    refactor: organize project files

--- src/main/factories/usecases/account/add-account/db-add-account-factory.ts
+++ src/main/factories/usecases/account/add-account/db-add-account-factory.ts
@@ -1,7 +1,7 @@
 import { BCryptAdapter } from '@/infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
-import { DbAddAccount } from '@/data/usecases/add-account/db-add-account'
-import { AddAccount } from '@/domain/usecases/add-account'
+import { DbAddAccount } from '@/data/usecases/account/add-account/db-add-account'
+import { AddAccount } from '@/domain/usecases/account/add-account'
 
 export const makeDbAddAccount = (): AddAccount => {
   const salt = 12
