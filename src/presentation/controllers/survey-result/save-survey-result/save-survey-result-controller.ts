24781362fc35960a4f5a166feff59099b982f013
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2021-01-15 14:55:37 -0300

    feat: ensure SaveSurveyResultController returns 403 if an invalid answer is provided

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,19 +1,29 @@
 import { InvalidParamError } from '@/presentation/errors'
 import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
-import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './save-survey-result-protocols'
+import { Controller, HttpRequest, HttpResponse, LoadSurveyById, SurveyAnswer } from './save-survey-result-protocols'
 
 export class SaveSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
-      if (!survey) {
+      const survey = await this.loadSurveyById.loadById(httpRequest.params?.surveyId)
+      if (survey) {
+        if (this.isValidAnswer(survey.answers, httpRequest.body.answer)) {
+
+        } else {
+          return forbidden(new InvalidParamError('surveyId'))
+        }
+      } else {
         return forbidden(new InvalidParamError('surveyId'))
       }
       return null
     } catch (error) {
       return serverError(error)
     }
+  }
+
+  private isValidAnswer (answers: SurveyAnswer[], answer: string): SurveyAnswer {
+    return answers.find(surveyAnswer => surveyAnswer.answer === answer)
   }
 }
